6da66c6057995bc1ac819da6c962a8272a3679b3
Ariel Agayof <arielagayof@Ariels-MacBook-Air.local>
2024-02-08 14:17:53 +0200

    upload

--- Track.java
+++ Track.java
@@ -42,7 +42,15 @@ class Track {
     // Returns a string that represents the totalSeconds as "minutes:seconds",
     // Where seconds is always two digits. For example, "3:17" or "12:05".
     private String formattedDuration(int totalSeconds) {
-        //// replace the following statement with your code
-        return "";
+        
+        int min = totalSeconds / 60;
+        int sec = totalSeconds % 60;
+        String duration = "" + min + ":";
+        if (sec<10) {
+            duration += "0";
+        }
+        duration += sec;
+
+        return duration;
     }
 }
